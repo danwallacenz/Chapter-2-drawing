017dfc227b8fea3ba61c6fe8518af914c154bbfe
Dan Wallace <daniel.wallace@datacom.co.nz>
2014-04-25 17:31:50 +1200

    initial commit

---
+++ Chapter 2 - Drawing/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Chapter 2 - Drawing
+//
+//  Created by Daniel Wallace [DATACOM] on 25/04/14.
+//  Copyright (c) 2014 Daniel Wallace [DATACOM]. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Chapter 2 - DrawingTests/Chapter_2___DrawingTests.m
@@ -0,0 +1,34 @@
+//
+//  Chapter_2___DrawingTests.m
+//  Chapter 2 - DrawingTests
+//
+//  Created by Daniel Wallace [DATACOM] on 25/04/14.
+//  Copyright (c) 2014 Daniel Wallace [DATACOM]. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Chapter_2___DrawingTests : XCTestCase
+
+@end
+
+@implementation Chapter_2___DrawingTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
