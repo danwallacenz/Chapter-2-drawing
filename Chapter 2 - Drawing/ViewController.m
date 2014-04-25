f597e21bebd37e17477f3c95629b567e9ced08b4
Dan Wallace <daniel.wallace@datacom.co.nz>
2014-04-25 20:16:50 +1200

    mona lisa png added

--- Chapter 2 - Drawing/ViewController.m
+++ Chapter 2 - Drawing/ViewController.m
@@ -9,15 +9,52 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *marsImageView;
 
 @end
 
 @implementation ViewController
 
+- (IBAction)topSegmentedButton:(UISegmentedControl *)sender {
+    long selectedIndex = sender.selectedSegmentIndex;
+    switch (selectedIndex) {
+        case 0:
+            self.marsImageView.contentMode = UIViewContentModeScaleToFill;
+            break;
+        case 1:
+            self.marsImageView.contentMode = UIViewContentModeScaleAspectFit;
+            break;
+        case 2:
+            self.marsImageView.contentMode = UIViewContentModeScaleAspectFill;
+            break;
+        case 3:
+            self.marsImageView.contentMode = UIViewContentModeRedraw;
+            break;
+        case 4:
+            self.marsImageView.contentMode = UIViewContentModeCenter;
+            break;
+        case 5:
+            self.marsImageView.contentMode = UIViewContentModeTop;
+            break;
+        case 6:
+            self.marsImageView.contentMode = UIViewContentModeBottom;
+            break;
+            
+        default:
+            break;
+    }
+    [self.view setNeedsDisplayInRect:self.marsImageView.bounds];
+    
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.marsImageView.image = [UIImage imageNamed:@"396px-Mona_Lisa.png"];
+    
+//    self.marsImageView.autoresizingMask = (UIViewAutoresizingFlexibleBottomMargin|UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleLeftMargin|UIViewAutoresizingFlexibleRightMargin|UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleWidth);
 }
 
 - (void)didReceiveMemoryWarning
