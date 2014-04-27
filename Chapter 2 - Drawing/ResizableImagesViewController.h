6fefe3a4bbd687b082c303139dfc130c826b1667
Dan Wallace <daniel.wallace@datacom.co.nz>
2014-04-27 12:38:17 +1200

    add tiled image

---
+++ Chapter 2 - Drawing/ResizableImagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResizableImagesViewController.h
+//  Chapter 2 - Drawing
+//
+//  Created by Daniel Wallace [DATACOM] on 27/04/14.
+//  Copyright (c) 2014 Daniel Wallace [DATACOM]. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResizableImagesViewController : UIViewController
+
+@end
