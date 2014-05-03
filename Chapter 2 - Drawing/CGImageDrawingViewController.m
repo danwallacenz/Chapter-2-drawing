662adbd0acaa6655f6deaeb2fc16495c30fbe4d9
Dan Wallace <daniel.wallace@datacom.co.nz>
2014-05-04 11:46:23 +1200

    add any resolution image - a double resolution image

--- Chapter 2 - Drawing/CGImageDrawingViewController.m
+++ Chapter 2 - Drawing/CGImageDrawingViewController.m
@@ -11,6 +11,7 @@
 @interface CGImageDrawingViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *splitCGImageView;
 @property (weak, nonatomic) IBOutlet UIImageView *splitCGImageFlippedCorrectlyImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *anyResolutionImageSplitInHalfAndCompensatedForFlippingImageView;
 
 @end
 
@@ -36,6 +37,8 @@
     UIImage *splitImageUnflipped = [self createImageSplitInHalfAndCompensatedForFlipping];
     [self.splitCGImageFlippedCorrectlyImageView setImage:splitImageUnflipped];
     
+    UIImage *anyResolutionImageSplitInHalfAndCompensatedForFlipping = [self createAnyResolutionImageSplitInHalfAndCompensatedForFlipping];
+    [self.anyResolutionImageSplitInHalfAndCompensatedForFlippingImageView setImage:anyResolutionImageSplitInHalfAndCompensatedForFlipping];
 }
 
 - (UIImage *)createImageSplitInHalfAndFlipped
@@ -104,6 +107,34 @@
     CGImageRelease(marsRight);
     
     return marsSplit;
+}
+
+- (UIImage *)createAnyResolutionImageSplitInHalfAndCompensatedForFlipping
+{
+    
+    UIImage* mars = [UIImage imageNamed:@"Mars"];
+    CGSize sz = mars.size;
+    // Derive CGImage and use its dimensions to extract its halves
+    CGImageRef marsCG = [mars CGImage];
+    CGSize szCG = CGSizeMake(CGImageGetWidth(marsCG), CGImageGetHeight(marsCG));
+    CGImageRef marsLeft =
+    CGImageCreateWithImageInRect(
+                                 marsCG, CGRectMake(0,0,szCG.width/2.0,szCG.height));
+    CGImageRef marsRight =
+    CGImageCreateWithImageInRect(
+                                 marsCG, CGRectMake(szCG.width/2.0,0,szCG.width/2.0,szCG.height));
+    UIGraphicsBeginImageContextWithOptions(
+                                           CGSizeMake(sz.width*1.5, sz.height), NO, 0);
+    // The rest is as before, calling flip() to compensate for flipping
+    CGContextRef con = UIGraphicsGetCurrentContext();
+    CGContextDrawImage(con, CGRectMake(0,0,sz.width/2.0,sz.height),
+                       flip(marsLeft));
+    CGContextDrawImage(con, CGRectMake(sz.width,0,sz.width/2.0,sz.height),
+                       flip(marsRight));
+    UIImage* im = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    CGImageRelease(marsLeft); CGImageRelease(marsRight);
+    return im;
 }
 
 
