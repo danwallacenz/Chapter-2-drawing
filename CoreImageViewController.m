0057b577b7d53a0290009cc9bfce88d396658f44
Dan Wallace <daniel.wallace@datacom.co.nz>
2014-05-04 19:43:05 +1200

    add radial gradient mask to mona lisa

--- CoreImageViewController.m
+++ CoreImageViewController.m
@@ -9,7 +9,8 @@
 #import "CoreImageViewController.h"
 
 @interface CoreImageViewController ()
-@property (weak, nonatomic) IBOutlet UIView *imageView;
+
+@property (weak, nonatomic) IBOutlet UIImageView *mainImageView;
 
 @end
 
@@ -22,18 +23,49 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 
-    self.imageView.layer.masksToBounds = YES;
-    self.imageView.layer.borderColor = [UIColor redColor].CGColor;
-    self.imageView.layer.borderWidth = 2;
-
-
-
-
-
-
-
+    self.mainImageView.layer.masksToBounds = YES;
+    self.mainImageView.layer.borderColor = [UIColor blackColor].CGColor;
+    self.mainImageView.layer.borderWidth = 2;
+//    [self.mainImageView setImage: [UIImage imageNamed:@"396px-Mona_Lisa" ]]; // normal image
+    
+    [self.mainImageView setImage: [self createCIImage]];
+    
 }
 
-
+- (UIImage *) createCIImage
+{
+    
+    UIImage *monaLisa =  [UIImage imageNamed: @"396px-Mona_Lisa" ];
+    CIImage *coreImageMonaLisa = [[CIImage alloc] initWithCGImage: monaLisa.CGImage ];
+    CGRect monaLisaExtent = coreImageMonaLisa.extent;
+    
+    // first filter
+    CIFilter *grad = [CIFilter filterWithName:@"CIRadialGradient"];
+    CIVector *center = [CIVector vectorWithX: monaLisaExtent.size.width/2.0
+                                           Y: monaLisaExtent.size.height/2.0];
+    [grad setValue: center
+            forKey:@"inputCenter"];
+    [grad setValue: @85
+            forKey:@"inputRadius0"];
+    [grad setValue: @100
+            forKey:@"inputRadius1"];
+    
+    CIImage *gradImage =[grad valueForKey:@"outputImage"];
+    
+    // second filter
+    CIFilter *blend = [CIFilter filterWithName:@"CIBlendWithMask"];
+    [blend setValue: coreImageMonaLisa
+            forKey:@"inputImage"];
+    [blend setValue: gradImage
+            forKey:@"inputMaskImage"];
+    
+    // extract a bitmap
+    CGImageRef coreGraphicsImageRef = [[CIContext contextWithOptions: nil]
+                                       createCGImage: blend.outputImage
+                                       fromRect:monaLisaExtent];
+    
+    UIImage *result = [UIImage imageWithCGImage: coreGraphicsImageRef];
+    return result;
+}
 
 @end
