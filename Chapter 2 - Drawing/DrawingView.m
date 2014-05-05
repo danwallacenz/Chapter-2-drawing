4f82144fba822538cc5577e23c935e4e457ec66e
Dan Wallace <daniel.wallace@datacom.co.nz>
2014-05-05 17:23:11 +1200

    draw another arrow using UIKit

--- Chapter 2 - Drawing/DrawingView.m
+++ Chapter 2 - Drawing/DrawingView.m
@@ -25,32 +25,36 @@
 - (void)drawRect:(CGRect)rect
 {
     
-    [self drawArrowUsingCoreGraphics];
+//    [self drawArrowUsingCoreGraphics];
     
-//    [self drawArrowUsingUIKit];
+    [self drawArrowUsingUIKit];
+    
+    [self drawArrowUsingCoreGraphics];
     
 }
 
 - (void) drawArrowUsingUIKit
 {
+    
+    CGFloat OFFSET = -50.0;
     // shaft of the arrow
     UIBezierPath *p = [UIBezierPath bezierPath];
-    [p moveToPoint: CGPointMake(100, 100)];
-    [p addLineToPoint: CGPointMake(100, 19)];
+    [p moveToPoint: CGPointMake(100 + OFFSET, 100)];
+    [p addLineToPoint: CGPointMake(100 + OFFSET, 19)];
     [p setLineWidth: 20];
     [p stroke];
-    //point of the arrow
-    [[UIColor redColor] set];
+    // point of the arrow yellow
+    [[UIColor greenColor] set];
     [p removeAllPoints];
-    [p moveToPoint: CGPointMake(80, 25)];
-    [p addLineToPoint: CGPointMake(100, 0)];
-    [p addLineToPoint: CGPointMake(120, 25)];
+    [p moveToPoint: CGPointMake(80 + OFFSET, 25)];
+    [p addLineToPoint: CGPointMake(100 + OFFSET, 0)];
+    [p addLineToPoint: CGPointMake(120 + OFFSET, 25)];
     [p fill];
     // snip out triangle in the tail
     [p removeAllPoints];
-    [p moveToPoint: CGPointMake(90,101)];
-    [p addLineToPoint: CGPointMake(100, 90)];
-    [p addLineToPoint: CGPointMake(110, 101)];
+    [p moveToPoint: CGPointMake(90 + OFFSET,101)];
+    [p addLineToPoint: CGPointMake(100 + OFFSET, 90)];
+    [p addLineToPoint: CGPointMake(110 + OFFSET, 101)];
     [p fillWithBlendMode:kCGBlendModeClear alpha: 1.0];
 }
 
@@ -60,16 +64,17 @@
     
     // obtain the current graphics context
     CGContextRef con = UIGraphicsGetCurrentContext();
+    CGContextSetStrokeColorWithColor(con, [[UIColor blackColor] CGColor]);
     
     //draw a black (by default) verticle line, the shaft of the arrow
-    CGContextMoveToPoint(con,100, 100);
+    CGContextMoveToPoint(con, 100, 100);
     CGContextAddLineToPoint(con, 100, 19);
     CGContextSetLineWidth(con, 20);
     CGContextStrokePath(con);
     
     // draw a red triangle, the point of the arrow
     CGContextSetFillColorWithColor(con, [[UIColor redColor] CGColor]);
-    CGContextMoveToPoint(con,80, 25);
+    CGContextMoveToPoint(con, 80, 25);
     CGContextAddLineToPoint(con, 100, 0);
     CGContextAddLineToPoint(con, 120, 25);
     CGContextFillPath(con);
--- Chapter 2 - Drawing/DrawingViewController.m
+++ Chapter 2 - Drawing/DrawingViewController.m
@@ -32,6 +32,4 @@
     [self.view addSubview:drawingView];
 }
 
-
-
 @end
